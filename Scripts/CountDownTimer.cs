26a2d3d9cc7905ad411fd6a9effc62b90314581a
SumSmoker <sumsmoker@gmail.com>
2026-04-02 17:15:33 -0400

    Update CountDownTimer.cs

--- Scripts/CountDownTimer.cs
+++ Scripts/CountDownTimer.cs
@@ -11,23 +11,16 @@ public class CountdownTimer : MonoBehaviour
     {
         if (totalTime > 0)
         {
-            // Subtract elapsed time every frame
-            totalTime -= Time.deltaTime;
-
-            // Divide the time by 60
-            float minutes = Mathf.FloorToInt(totalTime / 60);
-
-            // Returns the remainder
-            float seconds = Mathf.FloorToInt(totalTime % 60);
-
-            // Set the text string
-            timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+            totalTime -= Time.deltaTime; // Subtract elapsed time every frame
+            float minutes = Mathf.FloorToInt(totalTime / 60); // Divide the time by 60
+            float seconds = Mathf.FloorToInt(totalTime % 60); // Returns the remainder
+            timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds); // Set the text string
         }
         else
         {
             timerText.text = "00:00";
             totalTime = 0;
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //this will later be set to exit the level and go back to level select scene
         }
     }
 }
